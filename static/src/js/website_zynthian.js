5fe168ede1a65f899d21e19a39fe6d15fa0c42ad
vandekul <susannafm@gmail.org>
2020-06-28 18:23:44 +0200

    Zynthian Themes:  banner in product and "Sold out" product

--- static/src/js/website_zynthian.js
+++ static/src/js/website_zynthian.js
@@ -1,8 +1,19 @@
 odoo.define('website.website', function (require) {
+"use strict";
 /* --- Locate the "[!]" string and show the alert banner --- */
-
-if ($('h1:contains("[!]")').length>0 || $('table#cart_products:contains("[!]")').length>0) {
-  $('.zynthian_alert_message').fadeIn(1000);
-}
+console.log("ZYNTHIAN DIRTY TRICKS!");
+$(window).on('load',function() {
+  /* --- Locate the "[!]" string and show the alert banner --- */
+  if ($('h1:contains("[!]")').length>0 ||
+$('table#cart_products:contains("[!]")').length>0) {
+    $('.zynthian_alert_message').fadeIn(1000);
+  }
+  /* --- Locate the "[*]" string and remove "AddToCart button, replacing
+by "SOLD OUT" warning --- */
+  if ($('h1:contains("[*]")').length>0) {
+    $('#add_to_cart').hide();
+    $('#sold_out_warning').show();
+  }
+});
 
 });
