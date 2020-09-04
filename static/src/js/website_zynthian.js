f26ad569c29b64c134b6b64eaf5bdab5793592a0
vandekul <susannafm@gmail.org>
2020-09-04 19:16:14 +0200

    Delete "card icon" when product is not available by [*] symbol in product catalog.

--- static/src/js/website_zynthian.js
+++ static/src/js/website_zynthian.js
@@ -4,16 +4,20 @@ odoo.define('website.website', function (require) {
 console.log("ZYNTHIAN DIRTY TRICKS!");
 $(window).on('load',function() {
   /* --- Locate the "[!]" string and show the alert banner --- */
-  if ($('h1:contains("[!]")').length>0 ||
-$('table#cart_products:contains("[!]")').length>0) {
+  if ($('h1:contains("[!]")').length>0 || $('table#cart_products:contains("[!]")').length>0) {
     $('.zynthian_alert_message').fadeIn(1000);
   }
-  /* --- Locate the "[*]" string and remove "AddToCart button, replacing
-by "SOLD OUT" warning --- */
+  /* --- Locate the "[*]" string and remove "AddToCart button, replacing by "SOLD OUT" warning --- */
   if ($('h1:contains("[*]")').length>0) {
     $('#add_to_cart').hide();
     $('#sold_out_warning').show();
   }
+  $('div.oe_product_cart').each(function() {
+    if ($(this).find("a[itemprop='name']:contains('[*]')").length>0) {
+      $(this).find("a.btn").hide()
+    }
+  });
 });
 
 });
+
